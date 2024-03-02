1f61a94b30b22d01846c29e5f76022a0063e0c21
weslleyromao <161023585+weslleyromao@users.noreply.github.com>
2024-03-02 12:10:17 -0800

    Primeira desafio do curso de C++
    Nessa aula eu conclui meu primeiro
    desafio em C++.  Fazendo tudo sozinho!

--- dasafio1.cpp
+++ dasafio1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-    // f(x) = x^2 - 3x +5.
+    // f(x) = x^2 - 3x + 5.
     // Crie um Algoritmo em C++, que tenha uma função que calcula o valor de "f(a)", em que "a" é um número real informado pelo usuário.
 
 float f(float x){
@@ -15,7 +15,7 @@ float f(float x){
 
 int main(){
     float a;
-    cout << "Digite um numero real: " << endl;
+    cout << "Digite um numero real: \n";
     cin >> a;
 
     float valorf = f(a);
